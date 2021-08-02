93a8b5ef69472214e16f0f76b372f3adf77a0dc7
Thadd Trinh <thadtrin@amazon.com>
2021-08-02 13:26:53 -0700

    chore: added unit tests and error handling

--- src/AmplifyMapLibreGeocoder.ts
+++ src/AmplifyMapLibreGeocoder.ts
@@ -2,38 +2,56 @@ import { Geo } from "@aws-amplify/geo";
 
 export const AmplifyGeocoder = {
   forwardGeocode: async (config) => {
-    const data = await Geo.searchByText(config.query, {
-      biasPosition: config.proximity,
-      searchAreaConstraints: config.bbox,
-      countries: config.countires,
-      maxResults: config.limit,
-    });
-    const features = data.map((result) => {
-      const { geometry, ...otherResults } = result;
-      return {
-        type: "Feature",
-        geometry: { type: "Point", coordinates: geometry.point },
-        properties: { ...otherResults },
-        place_name: otherResults.label,
-        text: otherResults.label,
-        center: geometry.point,
-      };
-    });
+    const features = [];
+    try {
+      const data = await Geo.searchByText(config.query, {
+        biasPosition: config.proximity,
+        searchAreaConstraints: config.bbox,
+        countries: config.countires,
+        maxResults: config.limit,
+      });
+
+      if (data) {
+        data.forEach((result) => {
+          const { geometry, ...otherResults } = result;
+          features.push({
+            type: "Feature",
+            geometry: { type: "Point", coordinates: geometry.point },
+            properties: { ...otherResults },
+            place_name: otherResults.label,
+            text: otherResults.label,
+            center: geometry.point,
+          });
+        });
+      }
+    } catch (e) {
+      console.error(`Failed to forwardGeocode with error: ${e}`);
+    }
+
     return { features };
   },
   reverseGeocode: async (config) => {
-    const data = await Geo.searchByCoordinates(config.query, {
-      maxResults: config.limit,
-    });
-    const { geometry, ...otherResults } = data;
-    const feature = {
-      type: "Feature",
-      geometry: { type: "Point", coordinates: geometry.point },
-      properties: { ...otherResults },
-      place_name: otherResults.label,
-      text: otherResults.label,
-      center: geometry.point,
-    };
-    return { features: [feature] };
+    const features = [];
+    try {
+      const data = await Geo.searchByCoordinates(config.query, {
+        maxResults: config.limit,
+      });
+
+      if (data && data.geometry) {
+        const { geometry, ...otherResults } = data;
+        features.push({
+          type: "Feature",
+          geometry: { type: "Point", coordinates: geometry.point },
+          properties: { ...otherResults },
+          place_name: otherResults.label,
+          text: otherResults.label,
+          center: geometry.point,
+        });
+      }
+    } catch (e) {
+      console.error(`Failed to reverseGeocode with error: ${e}`);
+    }
+
+    return { features };
   },
 };
