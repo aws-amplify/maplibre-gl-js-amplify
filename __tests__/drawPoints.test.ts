39628cabc93b3f411b2e0d2e50cb6bf37dc6035a
Thadd Trinh <thadtrin@amazon.com>
2022-01-07 11:35:49 -0800

    chore: update drawPoints unit tests

--- __tests__/drawPoints.test.ts
+++ __tests__/drawPoints.test.ts
@@ -2,9 +2,36 @@ import { Map as maplibreMap } from "maplibre-gl";
 import { MAP_STYLES } from "../src/constants";
 import { drawPoints } from "../src/drawPoints";
 
-jest.mock("maplibre-gl");
+const addSourceMock = jest.fn();
+const addLayerMock = jest.fn();
+const fitBoundsMock = jest.fn();
+
+jest.mock("maplibre-gl", () => {
+  return {
+    Map: jest.fn().mockImplementation(() => {
+      return {
+        addLayer: addLayerMock,
+        addSource: addSourceMock,
+        on: jest.fn(),
+        addImage: jest.fn(),
+        getBounds: jest.fn().mockImplementation(() => {
+          return {
+            extend: jest.fn(),
+          };
+        }),
+        fitBounds: fitBoundsMock,
+      };
+    }),
+  };
+});
 
 describe("drawPoints", () => {
+  beforeEach(() => {
+    addSourceMock.mockClear();
+    addLayerMock.mockClear();
+    fitBoundsMock.mockClear();
+  });
+
   test("drawPoints default options", () => {
     const map = new maplibreMap();
     drawPoints(
@@ -18,14 +45,30 @@ describe("drawPoints", () => {
       MAP_STYLES.ESRI_NAVIGATION
     );
 
-    const mockInstance = (maplibreMap as jest.Mock).mock.instances[0];
+    expect(addSourceMock.mock.calls[0][0]).toEqual("foo-source-points");
+    expect(addSourceMock.mock.calls[0][1].data.features.length).toEqual(2);
+    expect(addLayerMock).toHaveBeenCalledTimes(2);
+    expect(fitBoundsMock).toHaveBeenCalledTimes(1);
+  });
 
-    expect(mockInstance.addSource.mock.calls[0][0]).toEqual(
-      "foo-source-points"
+  test("drawPoints autoFit false", () => {
+    const map = new maplibreMap();
+    drawPoints(
+      "foo",
+      [
+        [-123.1187, 49.2819],
+        [-122.849, 49.1913],
+      ],
+      map,
+      {
+        autoFit: false,
+      },
+      MAP_STYLES.ESRI_NAVIGATION
     );
-    expect(
-      mockInstance.addSource.mock.calls[0][1].data.features.length
-    ).toEqual(2);
-    expect(mockInstance.addLayer).toHaveBeenCalledTimes(2);
+
+    expect(addSourceMock.mock.calls[0][0]).toEqual("foo-source-points");
+    expect(addSourceMock.mock.calls[0][1].data.features.length).toEqual(2);
+    expect(addLayerMock).toHaveBeenCalledTimes(2);
+    expect(fitBoundsMock).toHaveBeenCalledTimes(0);
   });
 });
