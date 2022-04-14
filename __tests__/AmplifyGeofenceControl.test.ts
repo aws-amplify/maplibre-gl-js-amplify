711ce000d44e22785fadaca1180c5c9c70189e78
thaddmt <68032955+thaddmt@users.noreply.github.com>
2022-04-14 09:58:06 -0700

    chore: Add fit zoom on geofence display (#149)
    * chore: update when geofences are displayed to fit them to the map
    
    * chore: fit bounds of map when displaying geofence
    
    * chore: update fitBounds to only run when bounds have changed, update arrow syntax
    
    * chore: update unit tests

--- __tests__/AmplifyGeofenceControl.test.ts
+++ __tests__/AmplifyGeofenceControl.test.ts
@@ -1,4 +1,4 @@
-import { Map as maplibreMap } from "maplibre-gl";
+import { Map, Map as maplibreMap } from "maplibre-gl";
 import { AmplifyGeofenceControl } from "../src/AmplifyGeofenceControl";
 import { getGeofenceFeatureFromPolygon } from "../src/geofenceUtils";
 import { AmplifyGeofenceControlUI } from "../src/AmplifyGeofenceControl/ui";
@@ -54,6 +54,18 @@ describe("AmplifyGeofenceControl", () => {
         drawPolygonGeofence: jest.fn(),
       };
     });
+
+    (Map as jest.Mock).mockImplementation(() => {
+      return {
+        getBounds: jest.fn().mockImplementation(() => {
+          return {
+            extend: jest.fn(),
+            contains: jest.fn(),
+          };
+        }),
+        fitBounds: jest.fn(),
+      };
+    });
   });
 
   const createMockControl = () => {
@@ -96,6 +108,7 @@ describe("AmplifyGeofenceControl", () => {
     expect(control._loadedGeofences["foobar"]).toBeDefined();
     expect(control._loadedGeofences["foobar"].geofenceId).toBe("foobar");
     expect(control._ui.updateGeofenceCount).toHaveBeenCalled();
+    expect(control._map.fitBounds).toHaveBeenCalled();
   });
 
   test("Create Geofence API error", async () => {
@@ -182,6 +195,7 @@ describe("AmplifyGeofenceControl", () => {
     await control.saveGeofence(control._editingGeofenceId);
     expect(control._loadedGeofences["foobar"]).toBeDefined();
     expect(control._loadedGeofences["foobar"].geofenceId).toBe("foobar");
+    expect(control._map.fitBounds).toHaveBeenCalled();
   });
 
   test("Save Geofence with empty string uses editing geofence id", async () => {
