fb4dfd0a09f909fce51ebe0232f7c4e027d1ed08
Shane Laymance <shane.laymance@gmail.com>
2022-07-06 14:19:12 -0700

    test: Add unit test for createMap (#162)
    * Add unit test for createMap
    
    * Remove unnecessary jest env header
    
    * Polyfill Worker for jsdom env

---
+++ __tests__/createMap.test.dom.ts
@@ -0,0 +1,45 @@
+import { Amplify } from "@aws-amplify/core"; 
+import { Geo } from "@aws-amplify/geo";
+import type { AmazonLocationServiceMapStyle } from "@aws-amplify/geo";
+ 
+import { createMap } from "../src/AmplifyMapLibreRequest";
+
+jest.mock("@aws-amplify/geo");
+
+describe('createMap', () => {
+  Amplify.Auth = {};
+  Amplify.Auth.currentCredentials = jest.fn().mockImplementation(() => {
+    return {
+      accessKeyId: "accessKeyId",
+      sessionToken: "sessionTokenId",
+      secretAccessKey: "secretAccessKey",
+      identityId: "identityId",
+      authenticated: true,
+      expiration: new Date(),
+    };
+  });
+
+  beforeEach(() => {
+    (Geo.getDefaultMap as jest.Mock).mockClear();
+  });
+
+  test("createMap returns a map object", async () => {
+    (Geo.getDefaultMap as jest.Mock).mockReturnValueOnce({
+      mapName: "map1ff111f1-staging",
+      region: "us-east-1",
+      style: "VectorEsriStreets"
+    } as AmazonLocationServiceMapStyle);
+
+    const el = document.createElement("div");
+    el.setAttribute("id", "map");
+    document.body.appendChild(el);
+
+    const map = await createMap({
+      container: "map",
+      center: [-123.1187, 49.2819],
+      zoom: 11,
+    });
+
+    expect(map).toBeDefined();
+  });
+});
---
+++ jest.setup.dom.ts
@@ -0,0 +1,7 @@
+/**
+ * This is a workaround to the problem of the jsdom library not supporting
+ * URL.createObjectURL. See https://github.com/jsdom/jsdom/issues/1721.
+ */
+if (typeof window.URL.createObjectURL === 'undefined') {
+  window.URL.createObjectURL = jest.fn();
+}
