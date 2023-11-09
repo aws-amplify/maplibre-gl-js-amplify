df9bd744c088049fc6ba09cf5b08456cbf6639d3
Thadd Trinh <thadtrin@amazon.com>
2023-11-09 12:14:28 -0800

    fix: Revert "fix: testing another change"
    This reverts commit 0aad2bd3504ef6e672bed2c58d7a06cd0454957b.

--- src/AmplifyMapLibreRequest.ts
+++ src/AmplifyMapLibreRequest.ts
@@ -39,7 +39,6 @@ export default class AmplifyMapLibreRequest {
     this.region = region;
     this.activeTimeout = null;
     this.refreshCredentialsWithRetry();
-    console.log('hello');
 
     Hub.listen('auth', (data) => {
       switch (data.payload.event) {
